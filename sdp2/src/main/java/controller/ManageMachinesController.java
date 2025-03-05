c91fc1cd25e99e802680227735ad28aed4620caf
AnthonyDavidHogent <anthony.david@student.hogent.be>
2025-03-05 16:10:10 +0100

    Beheer machines header css afgerond

--- sdp2/src/main/java/controller/ManageMachinesController.java
+++ sdp2/src/main/java/controller/ManageMachinesController.java
@@ -1,97 +1,94 @@
 package controller;
 
 import java.io.IOException;
-import java.util.Comparator;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import com.mysql.cj.conf.ConnectionUrlParser.Pair;
 
 import domein.machine.Machine;
-import io.github.palexdev.materialfx.controls.MFXTableColumn;
-import io.github.palexdev.materialfx.controls.MFXTableView;
-import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
-import io.github.palexdev.materialfx.theming.JavaFXThemes;
-import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
-import io.github.palexdev.materialfx.theming.UserAgentBuilder;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class ManageMachinesController {
-    @FXML
-    private VBox VboxManageMachines;
+        @FXML
+        private HBox rootLayout;
 
-    @FXML
-    private HBox rootLayout;
+        @FXML
+        private VBox VboxManageMachines;
 
-    @FXML
-    private MFXTableView<Machine> tableView;
+        @FXML
+        private TextField searchBar;
 
-    @FXML
-    private ImageView addButton;
+        @FXML
+        private ImageView addButton;
 
-    @FXML
-    private void initialize() {
-        try {
-            setupNavbar();
-            setupTable();
-            setupCallbacks();
-        } catch (Exception e) {
-            e.printStackTrace();
+        @FXML
+        private TableView<Machine> tableView;
+        @FXML
+        private TableColumn<Machine, String> siteColumn;
+        @FXML
+        private TableColumn<Machine, String> codeColumn;
+        @FXML
+        private TableColumn<Machine, String> locationColumn;
+        @FXML
+        private TableColumn<Machine, String> productInfoColumn;
+        @FXML
+        private TableColumn<Machine, String> productionStatusColumn;
+        @FXML
+        private TableColumn<Machine, String> statusColumn;
+        @FXML
+        private TableColumn<Machine, String> technicianColumn;
+        @FXML
+        private TableColumn<Machine, LocalDateTime> uptimeColumn;
+        @FXML
+        private TableColumn<Machine, Pair<LocalDateTime, String>> lastMaintenanceColumn;
+        @FXML
+        private TableColumn<Machine, Date> nextMaintenanceColumn;
+
+        @FXML
+        private void initialize() {
+                try {
+                        setupNavbar();
+                        setupTable();
+                        setupCallbacks();
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }
         }
-    }
 
-    private void setupNavbar() throws IOException {
-        Node sideBar = new FXMLLoader(getClass().getResource("/view/Navbar.fxml")).load();
-        rootLayout.getChildren().add(0, sideBar);
-    }
+        private void setupNavbar() throws IOException {
+                Node sideBar = new FXMLLoader(getClass().getResource("/view/Navbar.fxml")).load();
+                rootLayout.getChildren().add(0, sideBar);
+        }
 
-    private void setupTable() {
-        MFXTableColumn<Machine> nameColumn = new MFXTableColumn<>("Site", true,
-                Comparator.comparing(Machine::getSiteNaam));
-        MFXTableColumn<Machine> codeColumn = new MFXTableColumn<>("Code", true, Comparator.comparing(Machine::getCode));
-        MFXTableColumn<Machine> locationColumn = new MFXTableColumn<>("Locatie", true,
-                Comparator.comparing(Machine::getLocatie));
-        MFXTableColumn<Machine> productInfoColumn = new MFXTableColumn<>("Product Info", true,
-                Comparator.comparing(Machine::getProductInfo));
-        MFXTableColumn<Machine> statusColumn = new MFXTableColumn<>("Status", true,
-                Comparator.comparing(Machine::getStatus));
-        MFXTableColumn<Machine> productionStatusColumn = new MFXTableColumn<>("Productie Status", true,
-                Comparator.comparing(Machine::getProductieStatus));
-        MFXTableColumn<Machine> uptimeColumn = new MFXTableColumn<>("Uptime", true,
-                Comparator.comparing(Machine::getUptime));
-        MFXTableColumn<Machine> technicianColumn = new MFXTableColumn<>("Technieker", true,
-                Comparator.comparing(Machine::getTechniekerNaam));
-        MFXTableColumn<Machine> lastMaintenanceColumn = new MFXTableColumn<>("Laatste Onderhoud", true,
-                Comparator.comparing((obj) -> obj.getAantalDagenSindsLaatsteOnderhoud()));
-        MFXTableColumn<Machine> nextMaintenanceColumn = new MFXTableColumn<>("Volgend Onderhoud", true,
-                Comparator.comparing(Machine::getDatumToekomstigeOnderhoud));
+        private void setupTable() {
+                siteColumn.setCellValueFactory(new PropertyValueFactory<>("siteNaam"));
+                codeColumn.setCellValueFactory(new PropertyValueFactory<>("code"));
+                locationColumn.setCellValueFactory(new PropertyValueFactory<>("locatie"));
+                productInfoColumn.setCellValueFactory(new PropertyValueFactory<>("productInfo"));
+                statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+                productionStatusColumn.setCellValueFactory(new PropertyValueFactory<>("productieStatus"));
+                uptimeColumn.setCellValueFactory(new PropertyValueFactory<>("uptime"));
+                technicianColumn.setCellValueFactory(new PropertyValueFactory<>("techniekerNaam"));
+                lastMaintenanceColumn.setCellValueFactory(new PropertyValueFactory<>("laatsteOnderhoud"));
+                lastMaintenanceColumn.setCellValueFactory(new PropertyValueFactory<>("datumToekomstigeOnderhoud"));
+        }
 
-        nameColumn.setRowCellFactory(machine -> new MFXTableRowCell<>(Machine::getSiteNaam));
-        codeColumn.setRowCellFactory(machine -> new MFXTableRowCell<>(Machine::getCode));
-        locationColumn.setRowCellFactory(machine -> new MFXTableRowCell<>(Machine::getLocatie));
-        productInfoColumn.setRowCellFactory(machine -> new MFXTableRowCell<>(Machine::getProductInfo));
-        statusColumn.setRowCellFactory(machine -> new MFXTableRowCell<>(Machine::getStatus));
-        productionStatusColumn.setRowCellFactory(machine -> new MFXTableRowCell<>(Machine::getProductieStatus));
-        uptimeColumn.setRowCellFactory(machine -> new MFXTableRowCell<>(Machine::getUptime));
-        technicianColumn.setRowCellFactory(machine -> new MFXTableRowCell<>(Machine::getTechniekerNaam));
-        lastMaintenanceColumn.setRowCellFactory(machine -> new MFXTableRowCell<>(Machine::getLaatsteOnderhoud));
-        nextMaintenanceColumn
-                .setRowCellFactory(machine -> new MFXTableRowCell<>(Machine::getDatumToekomstigeOnderhoud));
+        private void setupCallbacks() {
+                addButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (event) -> addButtonCallback());
+        }
 
-        tableView.getTableColumns().addAll(nameColumn, codeColumn, locationColumn, productInfoColumn,
-                statusColumn, productionStatusColumn, uptimeColumn, technicianColumn, lastMaintenanceColumn,
-                nextMaintenanceColumn);
+        private void addButtonCallback() {
 
-    }
-
-    private void setupCallbacks() {
-        addButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (event) -> addButtonCallback());
-    }
-
-    private void addButtonCallback() {
-        
-    }
+        }
 }
