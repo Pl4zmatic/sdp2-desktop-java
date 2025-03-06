4eb50bbfa9f67651e7345c522959befc655e0e4e
Wout Schelstraete <wout.schelstraete@student.hogent.be>
2025-03-06 11:26:47 +0100

    observer verwijderd

--- sdp2/src/main/java/domein/machine/Observer.java
+++
@@ -1,5 +0,0 @@
-package domein.machine;
-
-public interface Observer {
-	public void update(int aantalDagenSindsLaatsteOnderhoud);
-}
--- sdp2/src/main/java/domein/machine/Subject.java
+++
@@ -1,7 +0,0 @@
-package domein.machine;
-
-public interface Subject {
-
-	public void addObserver(Observer observer);
-	public void removeObserver(Observer observer);
-}
