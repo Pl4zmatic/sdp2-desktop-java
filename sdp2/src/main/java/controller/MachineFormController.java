19fc44d21f92e9110df9eace2731ebc46b721eea
AnthonyDavidHogent <anthony.david@student.hogent.be>
2025-03-10 17:14:57 +0100

    velden checks toegevoegd | velden ui veranderingen toegevoegd

--- sdp2/src/main/java/controller/MachineFormController.java
+++ sdp2/src/main/java/controller/MachineFormController.java
@@ -1,20 +1,37 @@
 package controller;
 
 import java.io.IOException;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import domein.machine.Machine;
+import domein.machine.RunningState;
+import domein.machine.StoppedState;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import lombok.Getter;
 import lombok.Setter;
 
 public class MachineFormController {
+
+    @Getter
+    @Setter
+    private Machine machine;
 
     @Setter
     private HBox parent;
@@ -31,6 +48,8 @@ public class MachineFormController {
     private TextField machineLoc;
     @FXML
     private TextField technician;
+    @FXML
+    private VBox lastMaintenanceContainer;
     @FXML
     private DatePicker lastMaintenance;
     @FXML
@@ -53,6 +72,8 @@ public class MachineFormController {
     private RadioButton active;
     @FXML
     private RadioButton inactive;
+    @FXML
+    private ToggleGroup status;
 
     // production status
     @FXML
@@ -61,12 +82,17 @@ public class MachineFormController {
     private RadioButton maintenance;
     @FXML
     private RadioButton healthy;
+    @FXML
+    private ToggleGroup productionStatus;
 
     // buttons
     @FXML
     private Button save;
     @FXML
     private Button cancel;
+
+    //error checking
+    private List<String> errors = new ArrayList<>();
 
     @FXML
     private void initialize() {
@@ -82,15 +108,126 @@ public class MachineFormController {
             }
         });
 
-        machineCode.addEventHandler(MouseEvent.MOUSE_CLICKED, (event) -> editMachineCode());
+        machineCode.addEventHandler(MouseEvent.MOUSE_CLICKED, (event) -> editMachineCodeCallback());
+        machineCode.focusedProperty().addListener((event) -> checkTextField(machineCode, "Code is vereist."));
+
+        site.focusedProperty().addListener((event) -> checkTextField(site, "Site is vereist."));
+        machineLoc.focusedProperty().addListener((event) -> checkTextField(machineLoc, "Locatie is vereist."));
+        productInfo.focusedProperty().addListener((event) -> checkTextField(productInfo, "Product info is vereist."));
+
+        technician.textProperty().addListener((event) -> checkTechnician());
+
+        lastMaintenance.setOnAction((event) -> checkDate("Datum is vereist."));
+
+        save.setOnAction((event) -> saveMachine());
     }
 
     private void cancelCallback() throws IOException {
         this.parent.getChildren().remove(this.rootLayout);
     }
 
-    private void editMachineCode() {
+    private void editMachineCodeCallback() {
         machineCode.setStyle("-fx-background-color: white;");
-        machineCode.clear();
+        machineCode.setText("");
     }
+
+    private <T extends TextInputControl> void checkTextField(T source, String error) {
+        errors.remove(error);
+        if ((source.getText().isBlank() || source.getText().isEmpty()) && !source.isFocused()) {
+            source.setPromptText(error);
+            source.setStyle("-fx-prompt-text-fill: -bgRed;");
+            errors.add(error);
+        }
+    }
+
+    private void checkTechnician() {
+        if (!technician.getText().isBlank() && !technician.getText().isEmpty()) {
+            lastMaintenanceContainer.setDisable(false);
+            if (this.machine == null) {
+                details.setDisable(true);
+            }
+        } else {
+            lastMaintenanceContainer.setDisable(true);
+        }
+    }
+
+    private void checkDate(String error) {
+        errors.remove(error);
+        if (lastMaintenance.getValue() == null && !lastMaintenanceContainer.isDisabled()) {
+            lastMaintenance.setPromptText(error);
+            lastMaintenance.setStyle("-fx-prompt-text-fill: -bgRed;");
+            errors.add(error);
+        }
+    }
+
+    private void checkToggleGroup(ToggleGroup t, String error) {
+        errors.remove(error);
+        if(t.getSelectedToggle() == null) {
+            errors.add(error);
+        }
+    }
+
+    private void checkDateTimeField(TextField field, String error) {
+        if(field.getText().isEmpty() || field.getText().isBlank()) {
+            field.setText("00");
+        }
+
+        try {
+            Integer.parseInt(field.getText());
+        } catch (Exception e) {
+            errors.add(error);
+        }
+    }
+
+    private void checkAll() {
+        checkTextField(machineCode, "Code is vereist.");
+        checkTextField(site, "Site is vereist.");
+        checkTextField(machineLoc, "Locatie is vereist.");
+        checkTextField(productInfo, "Product info is vereist.");
+        checkDate("Datum is vereist.");
+        checkToggleGroup(productionStatus, "Productie status is vereist");
+        checkToggleGroup(status, "Status is vereist");
+        checkDateTimeField(hours, "Uren");
+    }
+
+    private void saveMachine() {
+        checkAll();
+        if (!errors.isEmpty()) {
+            //errors tonen
+            String errorNotification = "";
+            for (String err : errors) {
+                errorNotification += err + "\n";
+            }
+            new Alert(AlertType.ERROR, errorNotification, ButtonType.OK).showAndWait();
+        }
+        else {
+            machine.setCode(machineCode.getText());
+            machine.setLocatie(machineLoc.getText());
+            machine.setProductInfo(productInfo.getText());
+            machine.setUptimeInHours(Integer.parseInt(hours.getText()));
+
+            //set vorige onderhoud
+            if(!lastMaintenanceContainer.isDisable()) {
+                machine.setTechniekerNaam(technician.getText());
+                machine.setLaatsteOnderhoudDatum(lastMaintenance.getValue().atStartOfDay());
+            }
+            
+            //set volgende onderhoud
+            if(!(nextMaintenance.getValue() == null)) {
+                machine.setDatumToekomstigeOnderhoud(Date.from(nextMaintenance.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
+            }
+            
+            //set status (actief/inactief)
+            if(((RadioButton) status.getSelectedToggle()).equals(active)) {
+                machine.setCurrentState(new RunningState(machine));
+            }
+            else {
+                machine.setCurrentState(new StoppedState(machine));
+            }
+            
+            //set productie status (gezond / (nood aan) onderhoud / falend)
+            machine.setProductieStatus(((RadioButton) status.getSelectedToggle()).getText());
+        }
+    }
+    
 }
