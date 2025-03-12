b8a2f1800d73ee9b79d606a33286f0f6c756f754
AnthonyDavidHogent <anthony.david@student.hogent.be>
2025-03-12 17:02:01 +0100

    machine editen + zoekfunctie compleet

--- sdp2/src/main/java/controller/MachineFormController.java
+++ sdp2/src/main/java/controller/MachineFormController.java
@@ -57,6 +57,8 @@ public class MachineFormController {
 
     // text fields
     @FXML
+    private VBox machineCodeContainer;
+    @FXML
     private TextField machineCode;
     @FXML
     private TextField site;
@@ -110,15 +112,29 @@ public class MachineFormController {
     // error checking
     private List<String> errors = new ArrayList<>();
 
+    private boolean isEditingFlag;
+
     @FXML
     private void initialize() {
         machineService = new MachineService();
         setupCallbacks();
         setupScrollPane();
+        isEditingFlag = false;
 
         // Set default form title
         if (formTitle != null) {
             formTitle.setText("Machine Form");
+        }
+    }
+
+    protected void setupSaveOption() {
+        if(machine != null) {
+            save.setText("Pas Aan");
+            isEditingFlag = true;
+            machineCodeContainer.setDisable(true);
+        }
+        else {
+            isEditingFlag = false;
         }
     }
 
@@ -231,7 +247,10 @@ public class MachineFormController {
     }
 
     private void checkAll() {
-        checkMachineCode("Code moet uniek zijn.");
+        if(!isEditingFlag) {
+            checkMachineCode("Code moet uniek zijn.");
+        }
+
         checkTextField(machineCode, "Code is vereist.");
         checkTextField(site, "Site is vereist.");
         checkTextField(machineLoc, "Locatie is vereist.");
@@ -287,7 +306,14 @@ public class MachineFormController {
             // set productie status (gezond / (nood aan) onderhoud / falend)
             machine.setProductieStatus(((RadioButton) productionStatus.getSelectedToggle()).getText());
 
-            machineService.addMachine(machine);
+            //Edit or add machine to db
+            if(isEditingFlag) {
+                machineService.update(machine);
+            }
+            else {
+                machineService.addMachine(machine);
+            }
+
             cancelCallback();
             this.parentController.selectMachine(machine);
         }
