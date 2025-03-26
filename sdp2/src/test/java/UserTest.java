f833ba473f86a3eee162fc3ded142aeef85918a8
Wout Schelstraete <wout.schelstraete@student.hogent.be>
2025-03-26 18:50:59 +0100

    testen en vpp

--- sdp2/src/test/java/UserTest.java
+++ sdp2/src/test/java/UserTest.java
@@ -13,7 +13,7 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-public class UserTest {
+class UserTest {
 
     private User user;
 
